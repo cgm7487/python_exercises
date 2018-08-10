985ecaa897b34df2660fc80856c034859eb7f1f4
cgm <cgm7487@gmail.com>
2018-08-10 17:59:12 +0800

    changed to pie chart

--- matplotlib/tax/tax_city_2016.py
+++ matplotlib/tax/tax_city_2016.py
@@ -2,5 +2,8 @@ import matplotlib.pyplot as plt
 from tabula import read_pdf
 
 df = read_pdf('105_6-1.pdf')
-df.loc[:,'稅後所得'].drop([0,1]).astype(int).plot(kind='bar', stacked=True, x=df.iloc[:,0].drop([0,1]))
+
+print(df.loc[2:23,'Unnamed: 0'])
+#df.loc[:'連江縣','稅後所得'].drop([0,1]).astype(int).plot(kind='bar')
+df.loc[2:23,'稅後所得'].astype(int).plot(kind='pie', labels=df.loc[2:23,'Unnamed: 0'])
 plt.show()
