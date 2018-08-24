ebd608d5581470fe5f53e88724c5481df7149296
cgm7487 <cgm7487@gmail.com>
2018-08-24 14:39:16 +0800

    fixed data type

--- gpsreader/__init__.py
+++ gpsreader/__init__.py
@@ -4,9 +4,17 @@ def get_gps_data(serialInterface, baudrate):
     with serial.Serial( serialInterface,
                         baudrate,
                         timeout = None) as gpsSer:
-        gpsData = gpsSer.readline()
+        gpsData = gpsSer.readline().decode('ascii')
 
         if gpsData[1:6] != "GPGGA":
             return "NODATA"
+
+        return gpsData
+
+def get_gps_raw_data(serialInterface, baudrate):
+    with serial.Serial( serialInterface,
+                        baudrate,
+                        timeout = None) as gpsSer:
+        gpsData = gpsSer.readline()
 
         return gpsData
